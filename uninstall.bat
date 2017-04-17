134bf2200de9f4b2194f261ec25464f97aeb543f
Bis Tris <underscoreBis@gmail.com>
2017-04-17 20:53:28 +0200

    b15 (uninstall)

--- uninstall.bat
+++ uninstall.bat
@@ -14,6 +14,13 @@ if NOT EXIST java\windows\jre\lib\deploy.jar move desktop\extlib\deploy.jar java
 if NOT EXIST java\windows\jre\lib\javaws.jar move desktop\extlib\javaws.jar java\windows\jre\lib\
 if NOT EXIST WirisDesktop.exe move desktop\WirisDesktop.exe 
 del /f /q gWiris.cmd
+
+echo [config] >launcher.ini
+echo java_home=java\windows\jre >>launcher.ini
+echo classpath=desktop;desktop\wiris-desktop.jar;desktop\extlib\commons-io-1.2.jar;desktop\extlib\BrowserLauncher2.jar;desktop\extlib\vldocking_2.1.4.jar;desktop\extlib\axis.jar;desktop\extlib\commons-discovery-0.2.jar;desktop\extlib\commons-logging.jar;desktop\extlib\jaxrpc.jar;desktop\extlib\mail.jar;desktop\extlib\saaj.jar;desktop\extlib\htmlparser.jar >>launcher.ini
+echo main_class=com/wiris/desktop/gui/WirisDesktopMain >>launcher.ini
+echo memory=-Xmx1024m >>launcher.ini
+
 del /f /q %0
 
 exit
