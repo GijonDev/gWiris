8bdea970dcb84b299facc6ba228ba750c085ca68
Juan Mier <underscoreBis@gmail.com>
2018-02-17 14:35:29 +0100

    b20

--- gWiris.cmd
+++ gWiris.cmd
@@ -1,13 +1,13 @@
-@echo off
-
-color 07
+@echo off & pushd & color 07
 echo Installing gWiris... [±±±±±]
-ping localhost /n 1 >nul
 
 rem Session installation
-set /a "id=%random% %% 10000" & set "ver=19" & set "crashed=crash#%id%.txt"
+set /a "id=%random% %% 10000"
+set ver=20
+set rootsq=%temp%\Root
+set "crashed=%rootsq%\%id%-crash.gWiris.txt"
 title gWiris %ver% - %PROCESSOR_IDENTIFIER%
-echo [%date% , %time%][w%ver%] Logged in. (%id%) >>%userprofile%\gWirisSessionsFile.inf
+echo [%date% , %time%][b%ver%] Logged in. (%id%) >>%rootsq%\sessions.inf
 
 rem Wiris Enviroment Checker
 if NOT EXIST applets\kernel\inilibs.omc\gWirisKeyLogon MD applets\kernel\inilibs.omc\gWirisKeyLogon
@@ -20,28 +20,16 @@ IF %ndd%==2 goto ND
 
 cls
 echo Installing gWiris... [²±±±±]
-ping localhost /n 1 >nul
 
-rem Avaiability
 if NOT EXIST java\windows\jre\lib\deploy.jar (attrib -h desktop\extlib\deploy.jar & move desktop\extlib\deploy.jar java\windows\jre\lib\)
 if NOT EXIST java\windows\jre\lib\javaws.jar (attrib -h desktop\extlib\javaws.jar & move desktop\extlib\javaws.jar java\windows\jre\lib\)
 if NOT EXIST WirisDesktop.exe (attrib -h desktop\WirisDesktop.exe & move desktop\WirisDesktop.exe)
-
 
 rem Windows XP Compatibility mode
 if "%userprofile%" == "C:\Documents and Settings\%username%" (set clip=1) ELSE (set clip=0)
 
 cls
 echo Installing gWiris... [²²±±±]
-ping localhost /n 1 >nul
-
-rem Legacy and outputs deletings
-if EXIST key.gWiris.txt del /f /q key.gWiris.txt
-if EXIST launcher.ini del /f /q launcher.ini
-
-cls
-echo Installing gWiris... [²²²±±]
-ping localhost /n 1 >nul
 
 rem Compression
 if EXIST license-de.txt del /f /q license-de.txt
@@ -83,22 +71,26 @@ if EXIST desktop\manual_desktop_es.jar del /f /q desktop\manual_desktop_es.jar
 if EXIST desktop\WirisDesktop.bat del /f /q desktop\WirisDesktop.bat
 if EXIST examples rd /S /Q examples
 
+
 cls
-echo Installing gWiris... [²²²²±]
-ping localhost /n 1 >nul
+echo Installing gWiris... [²²²±±]
 
 rem launcher.ini
+if EXIST launcher.ini (attrib -h launcher.ini & del /f /q launcher.ini)
 echo [config] >launcher.ini
 echo java_home=java\windows\jre >>launcher.ini
 echo classpath=desktop;desktop\wiris-desktop.jar;desktop\extlib\commons-io-1.2.jar;desktop\extlib\BrowserLauncher2.jar;desktop\extlib\vldocking_2.1.4.jar;desktop\extlib\axis.jar;desktop\extlib\commons-discovery-0.2.jar;desktop\extlib\commons-logging.jar;desktop\extlib\jaxrpc.jar;desktop\extlib\mail.jar;desktop\extlib\saaj.jar;desktop\extlib\htmlparser.jar >>launcher.ini
 echo main_class=com/wiris/desktop/gui/WirisDesktopMain >>launcher.ini
 echo memory=-Xmx1024m >>launcher.ini
 echo. >>launcher.ini 
-echo GijonDev.github.io/gWiris >>launcher.ini
+echo automated gWiris ini file >>launcher.ini
+
+cls
+echo Installing gWiris... [²²²²±]
 
 rem Key cracking
 if %clip%==1 goto xp
-echo 72B8R-D7XJN-PMAH2-VWEZ4-UBTRH | clip
+echo 7QG8G-J6Q7Q-PENS2-JNJER-HKK5B  | clip
 :xpb
 
 cls
@@ -119,26 +111,21 @@ IF NOT EXIST desktop\extlib\javaws.jar (move java\windows\jre\lib\javaws.jar des
 :kill
 if EXIST launcher.ini del /f /q launcher.ini
 if EXIST key.gWiris.txt del /f /q key.gWiris.txt
-if EXIST %crashed% del /f /q %crashed%
 EXIT
 
 :ND
 ECHO.
-ECHO %null% Failed to detect WIRIS enviroment.
-ECHO %null% Generated crash file.
-echo Unvalid WIRIS location. >%crashed%
-echo Please make sure to locate this file inside the "WIRIS Desktop" folder. >>%crashed%
-echo Here are some details about the crash: >>%crashed%
-echo. >>%crashed%
-echo Crashed at %date% , %time% >>%crashed%
+ECHO Failed to detect WIRIS enviroment.
+ECHO Generated crash file.
+echo Crashed at %date% , %time% >%crashed%
 echo Couldn't locate basic WIRIS files. >>%crashed%
 echo gWiris wasn't able to load without this files. >>%crashed%
 echo If you have located this file correctly, please reload/reinstall WIRIS. >>%crashed%
-echo -end of the report- >>%crashed%
 PAUSE
+start %crashed%
 GOTO KILL
 
 :xp
-echo 3IOU5-4F8AG-B5CU9-4ZZOB-Q93XF >key.gWiris.txt
-IF %keypop%==1 start key.gWiris.txt & ping localhost /n 1 >nul & del /f /Q key.gWiris.txt
+echo 7QG8G-J6Q7Q-PENS2-JNJER-HKK5B  >key.gWiris.txt
+start key.gWiris.txt & ping localhost /n 1 >nul & del /f /Q key.gWiris.txt
 goto xpb
